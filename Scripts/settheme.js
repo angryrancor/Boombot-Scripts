37d8f3bd6e0e47acac4a673b65c7618fdd9ee753
angryrancor <angryrancor@dissentingmedia.com>
2013-04-13 20:22:20 -0300

    Create settheme.js
    Simple room theme command.

---
+++ Scripts/settheme.js
@@ -0,0 +1,13 @@
+/**/// Description: Gets theme
+/**///
+/**/// Dependencies: None
+/**///
+/**/// Author: angyrancor
+/**///
+/**/// Notes: Sets the theme.  Usage: settheme <theme>
+exports.trigger = 'settheme';
+exports.listed = true;
+exports.script = function(boombot, text, uname, uid, private) {
+  boombot.theme = text.substring(9);
+  boombot.respond(uid, 'Current theme: '+boombot.theme+'.', private);
+}
