68553b60a67c31d660cb4158e021ac4a72e90946
angryrancor <angryrancor@dissentingmedia.com>
2013-04-29 00:25:18 -0300

    Updated so only mods can set the theme.

--- Scripts/settheme.js
+++ Scripts/settheme.js
@@ -7,7 +7,15 @@
 /**/// Notes: Sets the theme.  Usage: settheme <theme>
 exports.trigger = 'settheme';
 exports.listed = true;
-exports.script = function(boombot, text, uname, uid, private) {
-  boombot.theme = text.substring(9);
-  boombot.respond(uid, 'Current theme: '+boombot.theme+'.', private);
+exports.script = function(boombot, text, uname, uid, private) 
+{
+  boombot.bot.roomInfo(true, function(data) 
+	{
+        var modArray = data.room.metadata.moderator_id
+		if (modArray.indexOf(uid) !== -1) 
+		{
+			boombot.theme = text.substring(9);
+			boombot.respond(uid, 'Current theme: '+boombot.theme+'.', private);
+		}
+	})
 }
